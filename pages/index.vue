aa2741dc8fbc7f6619581f11ea4e61e341d2868b
adrian <campos.ajcs@gmail.com>
2024-12-27 15:17:38 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <h3>
+        <NuxtLink :to="'/pdf'">
+            Go to pdf
+        </NuxtLink>
+    </h3>
+</template>
---
+++ pages/pdf.vue
@@ -0,0 +1,36 @@
+<template>
+    <h3>
+        <NuxtLink :to="'/'">
+            Go home
+        </NuxtLink>
+        <div v-for="(page, index) in pages">
+            <VuePDF :pdf="pdf" :page="page"></VuePDF>
+        </div>
+       
+    </h3>
+</template>
+
+<script setup>
+
+import { VuePDF, usePDF } from '@tato30/vue-pdf';
+
+
+const { pdf, pages } = usePDF('https://retailgptstorage.blob.core.windows.net/retaildocs/walmart-supply-chain-packaging-guide-20240131.pdf');
+
+onUnmounted(async () => {
+
+if (pdf.value && typeof pdf.value.destroy === 'function') {
+  console.log('Destroying PDF document');
+  await pdf.value.destroy();
+}
+
+if (pdf.value && pdf.value._worker && pdf.value._worker._webWorker) {
+  console.log('worker terminated');
+  pdf.value._worker._webWorker.terminate(); // Terminate the worker manually
+} else {
+  console.log('could not find worker', pdf.value);
+}
+
+pdf.value = null;
+})
+</script>
